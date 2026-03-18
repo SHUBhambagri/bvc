77d1d0beb9bb037d6d4c2b986c38d8e52e090a2b
SHUBhambagri <sbagri2304@gmail.com>
2026-03-18 14:20:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -195,11 +195,10 @@ class PaymentTransaction(BaseModel):
 
 # ===== HELPER FUNCTIONS =====
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
 def get_password_hash(password):
+    password = password[:72]  # bcrypt limit fix
     return pwd_context.hash(password)
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
